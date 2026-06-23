b8743842ecac4af11440c133e7d17ab2b29d5245
Yehuda <yeudakatz@gmail.com>
2026-06-23 10:13:12 +0300

    Add	books	dictionary	with	5	books

--- library.py
+++ library.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 # ========== DATA ==========
-books = {}
+books = books	=	{ "B001":	{"title":	"Harry	Potter",	"author":	"J.K.	Rowling",	"available":	True}, "B002":	{"title":	"The	Hobbit",	"author":	"J.R.R.	Tolkien",	"available":	True}, "B003":	{"title":	"1984",	"author":	"George	Orwell",	"available":	True}, "B004":	{"title":	"The	Little	Prince",	"author":	"Antoine	de	Saint",	"available":	True}, "B005":	{"title":	"Pride	and	Prejudice",	"author":	"Jane	Austen",	"available":	True}, }
 readers = {}
 loans = {}
 
