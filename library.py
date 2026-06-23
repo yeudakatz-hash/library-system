ffc0b41dc8b4ef25552e28c6942cb9457497c83b
Yehuda <yeudakatz@gmail.com>
2026-06-23 10:20:22 +0300

    Add	initial	loans	dataוסידור של כל המילון

--- library.py
+++ library.py
@@ -1,9 +1,27 @@
 from datetime import datetime, timedelta
 
 # ========== DATA ==========
-books = books	=	{ "B001":	{"title":	"Harry	Potter",	"author":	"J.K.	Rowling",	"available":	True}, "B002":	{"title":	"The	Hobbit",	"author":	"J.R.R.	Tolkien",	"available":	True}, "B003":	{"title":	"1984",	"author":	"George	Orwell",	"available":	True}, "B004":	{"title":	"The	Little	Prince",	"author":	"Antoine	de	Saint",	"available":	True}, "B005":	{"title":	"Pride	and	Prejudice",	"author":	"Jane	Austen",	"available":	True}, }
-readers = {{ "R001":	{"name":	"David",	"books_borrowed":	[]}, "R002":	{"name":	"Yael",	"books_borrowed":	[]}, "R003":	{"name":	"Noam",	"books_borrowed":	[]}, "R004":	{"name":	"Shira",	"books_borrowed":	[]}, }
-loans = {}
+
+# ========== DATA ==========
+books = {
+    "B001": {"title": "Harry Potter", "author": "J.K. Rowling", "available": True},
+    "B002": {"title": "The Hobbit", "author": "J.R.R. Tolkien", "available": True},
+    "B003": {"title": "1984", "author": "George Orwell", "available": True},
+    "B004": {"title": "The Little Prince", "author": "Antoine de Saint", "available": True},
+    "B005": {"title": "Pride and Prejudice", "author": "Jane Austen", "available": True}
+}
+
+readers = {
+    "R001": {"name": "David", "books_borrowed": []},
+    "R002": {"name": "Yael", "books_borrowed": []},
+    "R003": {"name": "Noam", "books_borrowed": []},
+    "R004": {"name": "Shira", "books_borrowed": []}
+}
+
+loans = {
+    "B002": {"reader_id": "R001", "due_date": "2024-02-15"},
+    "B005": {"reader_id": "R002", "due_date": "2024-02-10"}
+}
 
 # ========== LOAN FUNCTIONS ==========
 # TODO: borrow_book(book_id, reader_id)
