1446251effe26b28c17a5af50b23e4405c8f0bbc
Yehuda <yeudakatz@gmail.com>
2026-06-23 10:02:18 +0300

    פתיחת קובץ library

---
+++ library.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+
+# ========== DATA ==========
+books = {}
+readers = {}
+loans = {}
+
+# ========== LOAN FUNCTIONS ==========
+# TODO: borrow_book(book_id, reader_id)
+# TODO: return_book(book_id)
+# TODO: extend_loan(book_id, days)
+
+# ========== REPORT FUNCTIONS ==========
+# TODO: search_book(title)
+# TODO: get_available_books()
+# TODO: get_overdue_loans()
+
+# ========== MAIN ==========
+if __name__ == "__main__":
+    print("Library System Ready")
