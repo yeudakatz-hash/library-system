95799040afaa1cf597d059dd3f560f604aaa0dc6
yedidyaf <jbh0527174650@gmail.com>
2026-06-23 10:21:00 +0300

    Add search_book function

--- library.py
+++ library.py
@@ -5,13 +5,21 @@ books = {}
 readers = {}
 loans = {}
 
+
 # ========== LOAN FUNCTIONS ==========
 # TODO: borrow_book(book_id, reader_id)
 # TODO: return_book(book_id)
 # TODO: extend_loan(book_id, days)
 
 # ========== REPORT FUNCTIONS ==========
-# TODO: search_book(title)
+def search_book(title):
+    results = []
+    for book_id, book in books.items():
+        if title.lower() in book["title"].lower():
+            results.append({"id": book_id, **book})
+    return results
+
+
 # TODO: get_available_books()
 # TODO: get_overdue_loans()
 
